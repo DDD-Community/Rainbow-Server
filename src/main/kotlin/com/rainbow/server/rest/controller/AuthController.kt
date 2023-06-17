fe9b529d6c011ba059d231575b4ed7335a2430fb
Jiwon-cho <gwon188@gmail.com>
2023-06-17 15:08:58 +0900

    Kakao login initial inbound setup

--- src/main/kotlin/com/rainbow/server/rest/controller/AuthController.kt
+++ src/main/kotlin/com/rainbow/server/rest/controller/AuthController.kt
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
+//import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -19,7 +19,7 @@ class AuthController(private val kakaoLoginService: KakaoLoginService,
                      @Value("\${oauth.kakao.client-id}")
                      private val clientId: String) {
 
-    @PostMapping("/kakao")
+    @GetMapping("/kakao")
     fun loginKakao(@RequestParam("code") code:String): ResponseEntity<Any> {
         return ResponseEntity.ok(kakaoLoginService.login(code))
     }

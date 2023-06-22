c90cdb5786d67119205064b1eada55b03f4a4fc5
Jiwon-cho <gwon188@gmail.com>
2023-06-22 21:23:48 +0900

    Add corsconfig

--- src/main/kotlin/com/rainbow/server/config/cors/CorsConfig.kt
+++ src/main/kotlin/com/rainbow/server/config/cors/CorsConfig.kt
@@ -11,7 +11,7 @@ class CorsConfig : WebMvcConfigurer {
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정 적용
-            .allowedOrigins("*") // 모든 도메인에 대해 액세스 허용
+            .allowedOriginPatterns("*") // 모든 도메인에 대해 액세스 허용
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true) // 자격 증명 허용

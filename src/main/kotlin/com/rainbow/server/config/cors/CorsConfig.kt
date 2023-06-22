6176f53edd81ec4c0eebcf9cf7f6e3349f4758f0
Jiwon-cho <gwon188@gmail.com>
2023-06-22 21:34:07 +0900

    Add corsconfig

--- src/main/kotlin/com/rainbow/server/config/cors/CorsConfig.kt
+++ src/main/kotlin/com/rainbow/server/config/cors/CorsConfig.kt
@@ -3,6 +3,7 @@ package com.rainbow.server.config.cors
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -16,5 +17,10 @@ class CorsConfig : WebMvcConfigurer {
             .allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true) // 자격 증명 허용
             .maxAge(3600) // CORS preflight 요청 결과 캐싱 시간 (초 단위)
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+        registry.addResourceHandler("/swagger-ui/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
     }
 }

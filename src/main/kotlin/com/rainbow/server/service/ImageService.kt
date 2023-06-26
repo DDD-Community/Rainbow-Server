32a744a862f8eeb2aa93437133a84a0cdeef09bf
Oraange <hrpp1300@naver.com>
2023-06-26 23:15:43 +0900

    Feat: S3 연결 코드 작성
    - 기존 로컬에 이미지 저장하던 방식에서 s3로 저장하는 방법으로 변경

--- src/main/kotlin/com/rainbow/server/service/ImageService.kt
+++ src/main/kotlin/com/rainbow/server/service/ImageService.kt
@@ -1,27 +1,43 @@
 package com.rainbow.server.service
 
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.services.s3.model.PutObjectRequest
+import com.amazonaws.util.IOUtils
 import com.rainbow.server.domain.image.Image
 import com.rainbow.server.domain.image.repository.ImageRepository
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayInputStream
 import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
+import java.io.IOException
 import java.util.*
 
 @Service
 class ImageService(
-    private val imageRepository: ImageRepository
+    private val imageRepository: ImageRepository,
+    private val s3Client: AmazonS3Client
 ) {
+    @Value("\${cloud.aws.s3.bucket}")
+    lateinit var bucket: String
+
+    @Value("\${cloud.aws.s3.dir}")
+    lateinit var dir: String
+
     @Transactional
     fun save(file: MultipartFile): Image {
         val originalFileName = file.originalFilename
         val saveFileName = genSaveFileName(originalFileName)
-        val filePath: String = System.getProperty("user.dir")!! // 임시 경로(추후 S3 경로로 변경)
-        val uploadDir = File(filePath).parent + "/images/"
-        Files.createDirectories(Paths.get(uploadDir))
-        uploadImage(uploadDir, file, saveFileName)
+
+        // TODO: S3 경로로 변경
+//        val filePath: String = System.getProperty("user.dir")!! // 임시 경로(추후 S3 경로로 변경)
+//        val uploadDir = File(filePath).parent + "/images/"
+//        Files.createDirectories(Paths.get(uploadDir))
+//        uploadImage(uploadDir, file, saveFileName)
+        upload(file, saveFileName)
 
         return imageRepository.save(Image(
             originalFileName = originalFileName,
@@ -35,11 +51,17 @@ class ImageService(
         return UUID.randomUUID().toString() + "." + ext
     }
 
-    private fun uploadImage(uploadDir: String, file: MultipartFile, saveFileName: String) {
-        println("file path: $uploadDir")
+    @Throws(IOException::class)
+    private fun upload(file: MultipartFile, saveFileName: String): String {
+        val objMeta = ObjectMetadata()
+        val bytes = IOUtils.toByteArray(file.inputStream)
+        objMeta.contentLength = bytes.size.toLong()
 
-        if (file != null && file.originalFilename != null) {
-            file.transferTo(File(uploadDir + saveFileName))
-        }
+        val byteArrayIs = ByteArrayInputStream(bytes)
+
+        s3Client.putObject(PutObjectRequest(bucket, dir + saveFileName, byteArrayIs, objMeta)
+            .withCannedAcl(CannedAccessControlList.PublicRead))
+
+        return s3Client.getUrl(bucket, dir + saveFileName).toString()
     }
 }
--- src/main/kotlin/com/rainbow/server/service/S3UploadService.kt
+++ src/main/kotlin/com/rainbow/server/service/S3UploadService.kt
@@ -1,23 +1,28 @@
-//package com.rainbow.server.service
-//
+package com.rainbow.server.service
+
+//import com.amazonaws.services.s3.AmazonS3Client
 //import com.rainbow.server.config.s3.AwsConfig
 //import com.amazonaws.util.IOUtils
+//import org.springframework.beans.factory.annotation.Value
 //import org.springframework.stereotype.Service
 //import org.springframework.web.multipart.MultipartFile
 //import java.io.IOException
 //import java.util.UUID
-//
+
 //@Service
-//class S3UploadService(private val awsS3Config: AwsConfig) {
+//class S3Service(
+//    private val s3Client: AmazonS3Client
+//) {
+//
+//    @Value("\${cloud.aws.s3.bucket}")
+//    lateinit var bucket: String
+//
+//    @Value("\${cloud.aws.s3.dir}")
+//    lateinit var dir: String
+//
 //    @Throws(IOException::class)
 //    fun upload(file: MultipartFile): String {
 //        val fileName = UUID.randomUUID().toString() + "-" + file.originalFilename
-//        val objMeta = objectMetadata()
-//        val bytes = IOUtils.toByteArray(file.inputStream)
-//        objMeta.contentLength = bytes.size.toLong()
 //
-//        val byteArrayIs = ByteArrayInputStream(bytes)
-//
-//        awsS3Config.amazonSQSAsync()
 //    }
 //}

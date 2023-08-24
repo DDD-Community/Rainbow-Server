7ff04d11244780a8ad1182fafb87d613a310fd34
Oraange <hrpp1300@naver.com>
2023-08-25 00:32:55 +0900

    Feat: Modify the code to reflect ktlint

--- src/main/kotlin/com/rainbow/server/rest/controller/ImageController.kt
+++ src/main/kotlin/com/rainbow/server/rest/controller/ImageController.kt
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import java.util.*
 
 @RestController
 @RequestMapping("/images")

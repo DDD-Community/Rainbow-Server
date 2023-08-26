7c4b82d9f7f9c3c0c00177ee12148df1e8899212
Oraange <hrpp1300@naver.com>
2023-08-26 15:58:40 +0900

    Feat: Add Property source to Aws Config

--- src/main/kotlin/com/rainbow/server/config/s3/AwsConfig.kt
+++ src/main/kotlin/com/rainbow/server/config/s3/AwsConfig.kt
@@ -10,8 +10,10 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
 
 @Configuration
+@PropertySource("classpath:application-local.yml")
 class AwsConfig {
 
     @Value("\${cloud.aws.credentials.access-key}")

3040fede872b5ad9c375c13d7a3b899e2a1c22da
Jiwon-cho <gwon188@gmail.com>
2023-08-26 12:35:08 +0900

    소스 정리

---
+++ src/main/kotlin/com/rainbow/server/rest/dto/image/ImageDto.kt
@@ -0,0 +1,9 @@
+// package com.rainbow.server.rest.dto.image
+//
+// import org.springframework.http.HttpStatus
+//
+// data class ErrorResponse(
+//     val status: Int,
+//     val error: HttpStatus,
+//     val message: String,
+// )
